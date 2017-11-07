ffea2ea83c3e70cfef85cbadae7d7dff07a68e15
MarrowOfAlbion <jerry.sam@ziggo.nl>
2017-11-07 19:00:00 +0100

    GoL code

---
+++ Cell.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	public enum States {
+		Dead, Alive
+	}
+
+	public Material livingMaterial;
+	public Material deadMaterial;
+
+	[HideInInspector] public GameOfLife gameOfLife;
+	[HideInInspector] public int x, y;
+	[HideInInspector] public Cell[] neighbours;
+
+	[HideInInspector] public States state;
+	private States nextState;
+
+	private MeshRenderer meshRenderer;
+
+	void Awake () {
+		meshRenderer = GetComponent <MeshRenderer> ();
+	}
+
+	// this method implements cells' behaviour
+	public void CellUpdate () {
+		nextState = state;
+		int aliveCells = GetAliveCells ();
+		if (state == States.Alive) { // if cell is alive
+			if (aliveCells != 2 && aliveCells != 3) // if cell less than 2 and more than 3 alive neighbours
+				nextState = States.Dead;
+		} else { // if cell if dead
+			if (aliveCells == 3) // if cell has 3 alive neighbours
+				nextState = States.Alive;
+		}
+	}
+
+	// apply new cell's state and update its material
+	public void CellApplyUpdate () {
+		state = nextState;
+		UpdateMaterial ();
+	}
+
+	// pass parent object and store x-axis and y-axis coordinates
+	public void Init (GameOfLife gol, int x, int y) {
+		gameOfLife = gol;
+		transform.parent = gol.transform;
+
+		this.x = x;
+		this.y = y;
+	}
+
+	// use it to set initial, random cell state
+	public void SetRandomState () {
+		state = (Random.Range (0, 2) == 0) ? States.Dead : States.Alive;
+		UpdateMaterial ();
+	}
+
+	// change cell appearance based on its state
+	private void UpdateMaterial () {
+		if (state == States.Alive)
+			meshRenderer.sharedMaterial = livingMaterial;
+		else
+			meshRenderer.sharedMaterial = deadMaterial;
+	}
+
+	// check cell's alive neighbours count
+	private int GetAliveCells () {
+		int ret = 0;
+		for (int i = 0; i < neighbours.Length; i++) {
+			if (neighbours[i] != null && neighbours [i].state == States.Alive)
+				ret++;
+		}
+		return ret;
+	}
+}
---
+++ GameOfLife.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameOfLife : MonoBehaviour {
+
+	public enum States {
+		Idle, Running
+	}
+
+	public Cell cellPrefab;
+
+	public float updateInterval = 0.1f; // delay between cell updates
+
+	[HideInInspector] public Cell[,] cells; // matrix of cells
+	[HideInInspector] public States state = States.Idle;
+	[HideInInspector] public int sizeX; // game size in x-axis
+	[HideInInspector] public int sizeY; // game size in y-axis
+
+	private Action cellUpdates; // action which calls cells' update methods
+	private Action cellApplyUpdates; // action which calls cells' apply update methods
+
+	private IEnumerator coroutine; // reference to main coroutine
+
+	void Awake () {
+		Init (50, 50); // init game with 50x50 cells
+
+		Run (); // start update coroutine
+	}
+
+	public void Init (int x, int y) {
+		// make sure that cells' matrix is empty and there is no cell object in the scene
+		if (cells != null) {
+			for (int i = 0; i < sizeX; i++) {
+				for (int j = 0; j < sizeY; j++) {
+					GameObject.Destroy (cells [i, j].gameObject);
+				}
+			}
+		}
+
+		// clear actions
+		cellUpdates = null;
+		cellApplyUpdates = null;
+
+		coroutine = null;
+
+		sizeX = x;
+		sizeY = y;
+		SpawnCells (sizeX, sizeY);
+	}
+
+	// this method invokes actions which call update and apply methods in cells
+	public void UpdateCells () {
+		cellUpdates ();
+		cellApplyUpdates ();
+	}
+
+	public void SpawnCells (int x, int y) {
+		cells = new Cell[x, y]; // create new cells' matrix
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				Cell c = Instantiate (cellPrefab, new Vector3 ((float)i, (float)j, 0f), Quaternion.identity) as Cell; // create new cell in scene
+				cells [i, j] = c;
+				c.Init (this, i, j); // init cell by passing this object to it
+				c.SetRandomState (); 
+				// register cell's methods to proper actions
+				cellUpdates += c.CellUpdate;
+				cellApplyUpdates += c.CellApplyUpdate;
+			}
+		}
+
+		// get and set references to neighbours for every cell
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				cells [i, j].neighbours = GetNeighbours (i, j);
+			}
+		}
+	}
+
+	// create array with adjacent cells to cell with coordinates (x,y)
+	public Cell[] GetNeighbours (int x, int y) {
+		Cell[] result = new Cell[8];
+		result[0] = cells[x, (y + 1) % sizeY]; // top
+		result[1] = cells[(x + 1) % sizeX, (y + 1) % sizeY]; // top right
+		result[2] = cells[(x + 1) % sizeX, y % sizeY]; // right
+		result[3] = cells[(x + 1) % sizeX, (sizeY + y - 1) % sizeY]; // bottom right
+		result[4] = cells[x % sizeX, (sizeY + y - 1) % sizeY]; // bottom
+		result[5] = cells[(sizeX + x - 1) % sizeX, (sizeY + y - 1) % sizeY]; // bottom left
+		result[6] = cells[(sizeX + x - 1) % sizeX, y % sizeY]; // left
+		result[7] = cells[(sizeX + x - 1) % sizeX, (y + 1) % sizeY]; // top left
+		return result;
+	}
+
+	// this method stops current coroutine and starts new its instance
+	public void Run () {
+		state = States.Running;
+		if (coroutine != null)
+			StopCoroutine (coroutine);
+		coroutine = RunCoroutine ();
+		StartCoroutine (coroutine);
+	}
+
+	private IEnumerator RunCoroutine () {
+		while (state == States.Running) { // while simulation is running
+			UpdateCells (); // update all cells in game
+			yield return new WaitForSeconds (updateInterval); // just wait...
+		}
+	}
+}
